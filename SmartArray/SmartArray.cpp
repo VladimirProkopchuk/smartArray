8a4bac5ce573da1176b1e8f9c41b1f0e907a0448
Vladimir Prokopchuk <vovan.devil999@gmail.com>
2024-05-16 16:10:33 +0300

    Initial commit

---
+++ SmartArray/SmartArray.cpp
@@ -0,0 +1,17 @@
+﻿#include "smartArray.h"
+
+int main()
+{
+    try {
+        smartArray arr(5);
+        arr.addElement(1);
+        arr.addElement(4);
+        arr.addElement(155);
+        arr.addElement(14);
+        arr.addElement(15);
+        std::cout << arr.getElement(2) << std::endl;
+    }
+    catch (const std::exception& ex) {
+        std::cout << ex.what() << std::endl;
+    }
+}
---
+++ SmartArray/smartArray.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <iostream>
+class smartArray
+{
+private:
+	int size;
+	int capacity;
+	int* arr;
+public:
+	smartArray(int _capacity) : size(0), capacity(_capacity) {
+		arr = new int[capacity];
+	}
+	~smartArray() {
+		delete[] arr;
+	}
+    void addElement(int element) {
+        if (size >= capacity) {
+            int newCapacity = 2 * capacity;
+            int* newArray = new int[newCapacity];
+            for (int i = 0; i < size; i++) {
+                newArray[i] = arr[i];
+            }
+            delete[] arr;
+            arr = newArray;
+            capacity = newCapacity;
+        }
+        arr[size] = element;
+        size++;
+    }
+    int getElement(int index) {
+        if (index < 0 || index >= size) {
+            std::cerr << "Invalid index" << std::endl;
+            return 0;
+        }
+        return arr[index];
+    }
+ 
+    }
+};
+
